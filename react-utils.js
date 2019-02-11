b536f529d5df807f0990082799ea75a436aa697d
ZJINA <1578711560@qq.com>
2019-02-11 14:23:36 +0800

    Update react-utils.js

--- react-utils.js
+++ react-utils.js
@@ -1,8 +1,11 @@
 
 //补零 from ant-design-pro
+//测试用例 '0',9，'9',09,'09',10,100,'0110'
 export function fixedZero(val) {
-  return val * 1 < 10 ? `0${val}` : `${val}`;
+  return val * 1 < 10 ? `0${val}` : val;
 }
 //补零 owner
-
+export function fixedZero(val) {
+  return val * 1 < 10 && val.toString().length<2? `0${val}` : `${val}`;
+}
 
